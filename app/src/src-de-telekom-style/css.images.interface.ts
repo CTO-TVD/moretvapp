802560dfed636eb0246591abbec4d2e11864e75e
Lukas Räßler <lukas.raessler@telekom.de>
2021-05-28 10:03:25 +0200

    Update build

--- app/src/src-de-telekom-style/css.images.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface ICssImages {
-
-    /**
-     * generated with: http://png-pixel.com/
-     * format: "data:image/png;base64,..."
-     */
-    readonly inlineTransparentPixel: string;
-}
